f9858e8a3c68ad63f12cc2e8b7d5958b770d5799
Artemio Guerrero <aguerrero2113@gmail.com>
2022-05-05 20:26:21 -0700

    modify calc/app.py

--- calc/app.py
+++ calc/app.py
@@ -1,64 +1,61 @@
-from flask import Flask, request
-from operations import add, sub, mult, div
+from flask import Flask,request
+from operations import add,sub,mult,div
 
 app = Flask(__name__)
 
-@app.route("/add")
-def do_add():
-    """Add a and b parameters."""
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    result = add(a, b)
-
-    return str(result)
-
-@app.route("/sub")
-def do_sub():
-    """Subtract and b parameters."""
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    result = sub(a, b)
+@app.route('/add')
+def addition():
+    """add a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a,b)
 
     return str(result)
 
-@app.route("/mult")
-def do_mult():
-    """Multiply a and b parameters."""
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    result = mult(a, b)
-
-    return str(result)
-
-@app.route("/div")
-def do_div():
-    """Divide a and b parameters."""
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    result = div(a, b)
+@app.route('/sub')
+def subtract():
+    """subtract a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
 
     return str(result)
 
-### PART TWO
+@app.route('/mult')
+def multiplication():
+    """multiply a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a,b)
+
+    return str(result)
+
+@app.route('/div')
+def divide():
+    """divide a and b"""
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a,b)
+
+    return str(result)
+
+# further study
 
 operators = {
-        "add": add,
-        "sub": sub,
-        "mult": mult,
-        "div": div,
-        }
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div,
+    }
 
-@app.route("/math/<oper>")
-def do_math(oper):
-    """Do math on a and b."""
+@app.route('/math/<oper>')
+def solve(oper):
 
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    result = operators[oper](a, b)
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = operators[oper](a,b)
 
     return str(result)
+
+
 
